7befa26a4f67e27795dedd0d1d43163ec65ab8eb
preeme <roa.michael5@gmail.com>
2018-01-09 23:42:45 -0800

    minor upodate

---
+++ app/components/popular.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+class Popular extends React.Component {
+  render() {
+    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul>
+        {languages.map((lang) => {
+          return (
+            <li> {lang} </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
+
+module.exports = Popular;
--- app/index.js
+++ app/index.js
@@ -1,16 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+var App = require('./components/app');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
+
 
 
 ReactDOM.render(
