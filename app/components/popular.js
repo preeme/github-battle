960de628539bf69350cd806be57a755b425faaef
preeme <roa.michael5@gmail.com>
2018-01-10 12:21:24 -0800

    added popular component with active tabbing

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,14 +1,35 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedLangauge: 'All'
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(() => {
+      return {
+        selectedLangauge: lang
+      }
+    });
+  }
+
   render() {
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return (
-      <ul>
+      <ul className='languages'>
         {languages.map((lang) => {
           return (
-            <li> {lang} </li>
+            <li
+              style={lang === this.state.selectedLangauge ? { color: '#d0021b'}: null}
+              onClick={this.updateLanguage.bind(null, lang)}
+              key={lang}>
+              {lang}
+            </li>
           )
         })}
       </ul>
