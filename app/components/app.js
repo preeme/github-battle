1db1caacaf8dbca0833250abfde0a42d3fe721d3
preeme <roa.michael5@gmail.com>
2018-01-11 13:19:45 -0800

    added home component

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,22 @@
 var React = require('react');
 var Popular = require('./popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./nav');
+var Home = require('./home');
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/' component={Home} />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
+
     )
   }
 }
