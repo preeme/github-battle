ebdfed6ab1c065679a19f9b3ca5cea8089a68a79
preeme <roa.michael5@gmail.com>
2018-01-13 11:44:00 -0800

    worked on some of the results

--- app/components/results.js
+++ app/components/results.js
@@ -1,10 +1,48 @@
 var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/api')
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+        let users = queryString.parse(this.props.location.search);
+        api.battle([
+          users.userOneName,
+          users.userTwoName
+        ]).then((results) => {
+          if (results === null) {
+            return this.setState(() => {
+              return {
+              error: 'Looks like there was an error. Check that both users exist on Github',
+              loading: false,
+            }
+            })
+          }
+        this.setState(() => {
+          return {
+            error: null,
+            winner: results[0],
+            loser: results[1],
+            loading: false
+
+          }
+        })
+        });
+  }
   render() {
+    let error = this.state.error;
+    let winner = this.state.winner;
+    let loser = this.state.loser;
+    let loading = this.state.loading;
+
     return (
       <div>
-        Results!
+        Results
       </div>
     )
   }
